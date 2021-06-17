e90199ea6832431211d7e06d4253ef0cedc45c57
Khiman Louer <30343764+alkanna@users.noreply.github.com>
2021-06-17 08:23:30 +0200

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 export const config = {
-  invite: 'NTBlMGQzMDAtY2E4ZS0xMWViLWJkNzEtZjFmMjgyZGY3NjVkOmYwYTE0NjdhLTMyYzEtMTFlYi1iMWI4LTViYTQzMmY1ZjBkMA==',
-  password: 'SvnKg7zFwkLc9mh9PAAWt',
+  invite: 'ZWNlNDUyZWItY2YzMy0xMWViLWFiNDEtYTM5ODRjNGRiMzJmOmYwYTE0NjdhLTMyYzEtMTFlYi1iMWI4LTViYTQzMmY1ZjBkMA==',
+  password: 'SvnKg7zFwkLc9mh9PAAXt',
   screenshotsDir: './screenshots',
   runs: 80,
 };
